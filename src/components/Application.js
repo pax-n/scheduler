d2a11beb0bcb7015e1f123a7e2384d4ec174541f
pax-n <paxton_nguyen@hotmail.com>
2022-02-24 20:19:40 -0500

    combined state

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,14 +46,24 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }))
+  };
+  
   useEffect(() => {
     axios.get(`/api/days`).then(response => {
-      setDay([response.data.results])
+      setDays(response.data)
       })
   }, [])
+
+  
 
   return (
     <main className="layout">
@@ -66,8 +76,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days} 
-          value={day} 
+          days={state.days} 
+          value={state.day} 
           onChange={setDay} 
         />
       </nav>
