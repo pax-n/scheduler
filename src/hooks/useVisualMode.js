8ca476a4f1c0651410be85d9eb33890d3f74e22f
pax-n <paxton_nguyen@hotmail.com>
2022-02-25 20:06:46 -0500

    newly built custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+  function transition(newMode, replace = false) {
+    
+    if (replace) {
+      setMode(newMode)
+    } else {
+      history.push(newMode);
+      setMode(newMode);
+    }
+  }
+
+  function back() {
+    if (history.length-2 >= 0) {
+      setMode(history[history.length-2]);
+      history.pop();
+    }
+  }
+  
+  return { mode, transition, back };
+}
+
